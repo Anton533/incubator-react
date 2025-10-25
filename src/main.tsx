7751238212b5f3aebe80cd679867f4ff037ec82b
Anton <anton.omelianenko533@gmail.com>
2025-10-25 17:42:24 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+
+const rootEle = document.getElementById("root");
+const reactRoot = createRoot(rootEle!);
+
+reactRoot.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
