3899beab80d2d2abd5a98b3c3101ad55e2615cab
Anton <anton.omelianenko533@gmail.com>
2025-10-27 20:40:42 +0200

    lesson-02

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
@@ -6,8 +5,4 @@ import App from "./App.tsx";
 const rootEle = document.getElementById("root");
 const reactRoot = createRoot(rootEle!);
 
-reactRoot.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+reactRoot.render(<App />);
